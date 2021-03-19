98f880b9f2ccaf8a5576223dbea889aee2351640
xenonflash <jzykc@hotmail.com>
2021-03-19 19:01:46 +0800

    devtool page init

---
+++ src/js/devtool.js
@@ -0,0 +1,4 @@
+chrome.devtools.panels.create("Vf Devtool", null, "/src/devtool/main-panel.html", function (panel) {
+  // code invoked on panel creation
+  console.log("created main devtool panel")
+})
